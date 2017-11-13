5a194d5793f55498bacaf82d95fa00a069b2f489
lmaciejak <LuizaMaciejak@ac.c4q.nyc>
2017-11-13 15:04:35 -0500

    edited exercises

--- exercisesES6/part1/part1_Q5.js
+++ exercisesES6/part1/part1_Q5.js
@@ -2,7 +2,6 @@ let sum = 0;
 const sumAll = (num) => sum += num 
 
 const forEachEl = (arr, func) => { 
-    return arr.map(elem => func(elem))
+   console.log(arr.map(elem => func(elem)))
 }
-
-console.log(forEachEl([1,2,3,11],sumAll))
+forEachEl([1,2,3,11],sumAll)
